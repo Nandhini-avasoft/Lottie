5aaafcf72deac344391c48dc58642d00ad7897c9
Nandhini-avasoft <97678199+Nandhini-avasoft@users.noreply.github.com>
2022-03-23 22:12:33 +0530

    Add files via upload

---
+++ LottieLoaders/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LottieLoaders.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LottieLoaders
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Application.Current.MainPage = new CreateLottieScreen();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
